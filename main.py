bbb936e39b031897a759e933d2de9679b86ea881
Nicolás Montesino <nicolasmontesino@outlook.com>
2023-01-15 21:47:42 +0000

    Infinite loop

--- main.py
+++ main.py
@@ -52,7 +52,8 @@ def collect_30_packets():
 
     i = 0
 
-    while i < 20:
+    while i < 5:
+        print(i)
         print("TUKA")
         try:
             payload, client = sock.recvfrom(4096)  # experimental, tested with 1464 bytes
@@ -234,22 +235,23 @@ def main_without_file():
     else:
         for i in range(2, len(sys.argv)):
             IP_ADDRESSES.append(sys.argv[i])
+    while(True): 
+        print("Iteration started")
+        data, raw_flows = collect_30_packets()
+            
+        res, aggregated = by_time_from_collector(data, seconds)
     
-    data, raw_flows = collect_30_packets()
-        
-    res, aggregated = by_time_from_collector(data, seconds)
-
-    cnt = 0
-
-    model = Doc2Vec.load("model.doc2vec")
-
-    classified = []
-
-    for key, value in aggregated.items():
-        for session in value:
-            classified.append((infer(model, list(session[0])), key))
-            print(list(session[0]))
-            print(infer(model, list(session[0])))
+        cnt = 0
+    
+        model = Doc2Vec.load("model.doc2vec")
+    
+        classified = []
+    
+        for key, value in aggregated.items():
+            for session in value:
+                classified.append((infer(model, list(session[0])), key))
+                print(list(session[0]))
+                print(infer(model, list(session[0])))
     # Here goes the injection to elk part
     # classified - a list of tuples (application, local_ip)
     # raw_flows - a list of tuples go to line 85 to see the format
